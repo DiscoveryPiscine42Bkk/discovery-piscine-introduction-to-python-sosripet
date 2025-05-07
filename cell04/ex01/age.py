b033a633492591790a1a9fcd34f474f856696c24
Soravis <64100309@kmitl.ac.th>
2025-05-08 00:36:26 +0700

    save

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+import sys
 greet = input("please tell me your age :")
-print(f"you are currently {greet} years old.")
+age_int = int(greet)
+if age_int<0:
+    print("Age must be a positive number. Please try again.")
+    sys.exit()
+print(f"you are currently {age_int} years old.")
+i = 10
+while i <= 30:
+    print(f"in{i}years, you will be {age_int + i}year sold")
+    i += 10    
+
+
